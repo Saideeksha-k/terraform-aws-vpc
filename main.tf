93df4c01c16df66546c88d68d39f420e5f66dda7
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-03-05 15:00:37 +0530

    terraform

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "main" {
   tags = local.vpc_final_tags
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id   #Association with vpc
 
   tags =local.igw_final_tags
@@ -90,4 +90,63 @@ resource "aws_route_table" "databse" {
     },
     var.database_route_table_tags
   )
+}
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = merge (
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-nat"
+    },
+    var.eip_tags
+  )
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.private[0].id
+
+  tags = merge (
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}"
+    },
+    var.nat_gateway_tags
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count=length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count=length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "databse" {
+  count=length(var.databse_subnet_cidrs)
+  subnet_id      = aws_subnet.databse[count.index].id
+  route_table_id = aws_route_table.databse.id
 }
