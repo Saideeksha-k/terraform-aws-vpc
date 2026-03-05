5b66be5c76ffcd42a7166b660c3552c9547fdec1
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-03-05 13:58:18 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -9,6 +9,10 @@ variable "project" {
 
 variable "environment" {
     type = string
+    validation {
+        condition = contains(["dev", "qa", "uat", "prod"], var.environment)
+        error_message = "Environment should be one of dev, qa,uat or prod"
+    }
 }
 
 variable "vpc_tags" {
@@ -26,6 +30,24 @@ variable "public_subnet_cidrs" {
     default = ["10.0.1.0/24" , "10.0.2.0/24"]
 }
 variable "public_subnet_tags" {
+    type = map
+    default= {}
+}
+
+variable "private_subnet_cidrs" {
+    type = list(string)
+    default = ["10.0.11.0/24" , "10.0.21.0/24"]
+}
+variable "private_subnet_tags" {
+    type = map
+    default= {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list(string)
+    default = ["10.0.21.0/24" , "10.0.22.0/24"]
+}
+variable "database_subnet_tags" {
     type = map
     default= {}
 }
