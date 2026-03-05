f05fafb07d4998c99a48e705d6def3e707277b88
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-03-05 12:59:54 +0530

    terraform

--- main.tf
+++ main.tf
@@ -5,3 +5,9 @@ resource "aws_vpc" "main" {
 
   tags = local.vpc_final_tags
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id   #Association with vpc
+
+  tags =local.igw_final_tags
+}
