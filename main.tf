8d70b10b135c140bd15e67c5f63b33535be21ef7
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-03-05 13:34:44 +0530

    terraform

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidrs
+  cidr_block        = var.public_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index] # Replace with a valid AZ
 
   tags = merge (
