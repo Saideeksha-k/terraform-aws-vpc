87fd982aef0672d5a6958ddbc7c1e46194925dd3
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-03-05 12:41:16 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "project" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "vpc_tags" {
+    type = map
+    default = {}
+}
